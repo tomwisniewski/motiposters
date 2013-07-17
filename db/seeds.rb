bfdee82ea1d43bd1ecc16798be76b5abb469a7fe
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-07-17 18:04:33 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create({:title => "Beard", :image => "moti1.jpg", :price => 57 })
+Product.create({:title => "Zombies", :image => "moti2.jpg", :price => 102 })
+Product.create({:title => "Good Things", :image => "moti3.jpg", :price => 12 })
+Product.create({:title => "Who Cares?", :image => "moti4.jpg", :price => 36 })
+Product.create({:title => "Super Dario", :image => "moti5.jpg", :price => 1000 })
