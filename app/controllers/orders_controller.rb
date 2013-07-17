710eab93f849d8ff7d7a4113d5b4c73c341651ca
tboeckmann <trb@bespecific.co.uk>
2013-07-17 12:26:28 +0100

    merge changes

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,14 @@
+require "stripe"
+
+
+
+class OrdersController < ApplicationController
+
+  def create
+
+    amount = Product.find(params[:product_id]).amount
+
+     
+    
+  end
+end
---
+++ spec/controllers/orders.rb
@@ -0,0 +1,11 @@
+require 'spec_helpers'
+
+describe 'order controller' do
+  
+  it "should create order" do
+    post :create, 
+      
+
+  end
+
+end
