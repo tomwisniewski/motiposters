3af5292316aac40db8dece292e458ccf1b89a105
James Lieu <j.lieu888@gmail.com>
2013-07-17 11:28:04 +0100

    Fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motiposters::Application.config.secret_key_base = '78340def91edcfc49397d38d3fe768738bbfc788326e7c3062755d62458e03efdf534267ee1cf43d9eedb45de873f517648bbddac3fc0894b67f668976b49667'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Motiposters::Application.config.session_store :cookie_store, key: '_motiposters_session'
