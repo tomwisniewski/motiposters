3429e1b322e30ce95d9a2c7774dc19be4dfc7f2c
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-07-18 12:23:14 +0100

    refactored orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,34 +2,32 @@ require "stripe"
 
 class OrdersController < ApplicationController
 
-  def create
-
-    @amount = Product.find(params[:product_id]).price
-
-    charge = Stripe::Charge.create(
-      :card  => params[:stripeToken],
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'GBP'
-    )
-
-    order = Order.create!(
-      :product_id => params[:product_id],
-      :name => params[:name],
-      :email => params[:email],
-      :street => params[:street],
-      :postcode => params[:postcode],
-      :city => params[:city]
-      )
-
-    redirect_to order_path(order.id)
-    
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
+  def create    
+    order = Order.new(order_params)
+    create_charge(params[:stripeToken], order.product.price)
+    order.save!
+    redirect_to order_path(order)    
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_url
   end
 
   def show
     @order = Order.find(params[:id])
-    @product = Product.find(@order.product_id)
+  end
+
+private
+
+  def order_params
+    params.require(:order).permit(:product_id, :name, :email, :street, :postcode, :city)
+  end
+
+  def create_charge(token, amount)
+    charge = Stripe::Charge.create(
+      :card        => token,
+      :amount      => amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'GBP'
+    )
   end
 end
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -7,14 +7,16 @@ module Helpers
   def order_params
     product = FactoryGirl.create(:product)
     {
-      product_id: product.id,
-      name: "John Smith",
-      email: "john@example.com",
-      street: "City Road",
-      postcode: "NW1 34Q",
-      city: "London", 
-      price: product.price,
-      stripeToken: "tok_u5dg20Gra"
+      :order => {
+        product_id: product.id,
+        name: "John Smith",
+        email: "john@example.com",
+        street: "City Road",
+        postcode: "NW1 34Q",
+        city: "London", 
+        price: product.price,
+        stripeToken: "tok_u5dg20Gra"
+      }
     }
   end
 
